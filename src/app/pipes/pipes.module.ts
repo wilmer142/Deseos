fbcac758ea98c3a405d5264333fae4fd888baba4
wilmer142 <wilmercollege@gmail.com>
2019-11-14 14:41:38 -0500

    Implementing pipe for manage changes on complete and uncomplete tasks

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FiltroCompletadoPipe } from './filtro-completado.pipe';
+
+
+@NgModule({
+  declarations: [FiltroCompletadoPipe],
+  imports: [],
+  exports: [
+    FiltroCompletadoPipe
+  ]
+})
+export class PipesModule { }

2c3c180bb25b2aa6bc63bc5cd9da8cfdfffbc03d
wilmer142 <wilmercollege@gmail.com>
2019-11-15 09:19:00 -0500

    Implementing edit task

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -10,10 +11,12 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class ListasComponent implements OnInit {
 
+  @ViewChild( IonList) lista: IonList;
   @Input() terminados = true;
 
   constructor(public deseosService: DeseosService,
-    private router: Router,) {
+              private router: Router,
+              private alertCtrl: AlertController) {
   }
 
   listaSeleccionada(lista: Lista) {
@@ -26,6 +29,44 @@ export class ListasComponent implements OnInit {
 
   borrarLista(lista:Lista) {
     this.deseosService.borrarLista(lista);
+  }
+
+  async editarLista(lista:Lista) {
+    // this.router.navigate(['/tabs', 'tab1', 'agregar']);
+
+    const alert = await this.alertCtrl.create({
+      header: 'Editar lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Editar',
+          handler: (data) => {
+            if (data.titulo.length === 0) {
+              return;
+            }
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
   ngOnInit() { }
